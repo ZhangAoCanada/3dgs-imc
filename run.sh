a103696625f1e5ac5968edffec88f8ce7f5d2de2
ZhangAoCanada <zhangaocanada@gmail.com>
2026-01-20 09:33:51 +0800

    redo the experiments

---
+++ run.sh
@@ -0,0 +1,80 @@
+# eps
+# ranges_scale
+# depth
+# random view
+# interval
+
+for sigma_scaling in 1e-3 1e-2 1e-1 1e0
+do
+    # for noise_method in sigma-detach opacity-sigma-detach 1-opacity-detach
+    for partial_scaling in 1e-2 1e0 1e1
+    do
+        nn_start_iter=2000
+        nn_derivatives_iter=$((nn_start_iter+500))
+        nn_update_interval=500
+        # nn_update_interval=1000
+        images=images
+        init_type=sfm
+        noise_lr=5e5
+        # cap_max=3000000
+        cap_max=2000000
+        scale_reg=0.01
+        opacity_reg=0.01
+        densify_from_iter=500
+        densification_interval=200
+        # depth_l1_weight_init=0.1
+        depth_l1_weight_init=0.3
+        # depth_l1_weight_init=0.5
+        depth_l1_weight_final=$(echo "scale=10; $depth_l1_weight_init * 0.01" | bc)
+        nn_type=allviews
+        bound_type=local
+        # partial_scaling=1e0
+        # partial_scaling=1e-2
+        # sigma_scaling=1e-3
+        # eps=0.05
+        eps=0.1
+        min_samples=500
+        minmax=wholeonly
+        range_scale=1
+        noise_method=sigma-detach
+        # noise_method=1-opacity-detach
+        # noise_method=opacity-sigma-detach
+        random_view_num=3
+        dvoxel_size=0.01
+        port=12311
+        echo "[*****training*****] training with noise_method=${noise_method}"
+        CUDA_VISIBLE_DEVICES=1 python train_experimental_branch3.py \
+            --source_path /data1/zhangao/DATA/large_angle/Zeche1/train \
+            --depths dav2 \
+            --test_path /data1/zhangao/DATA/large_angle/Zeche1/test \
+            --model_path outputs/Zeche1/experimental_branch3_${init_type}_${nn_type}_${bound_type}_${eps}_${min_samples}_${depth_l1_weight_init}_${depth_l1_weight_final}_${minmax}_${range_scale}_${partial_scaling}_${sigma_scaling}_${noise_method}_${random_view_num}_${scale_reg}_${opacity_reg}_${nn_start_iter}_${nn_derivatives_iter}_${nn_update_interval}_wocolor \
+            --images ${images} \
+            --resolution -1 \
+            --init_type ${init_type} \
+            --cap_max ${cap_max} \
+            --data_device cpu \
+            --scale_reg ${scale_reg} \
+            --opacity_reg ${opacity_reg} \
+            --noise_lr ${noise_lr} \
+            --densify_from_iter ${densify_from_iter} \
+            --densification_interval ${densification_interval} \
+            --depth_l1_weight_init ${depth_l1_weight_init} \
+            --depth_l1_weight_final ${depth_l1_weight_final} \
+            --nn_type ${nn_type} \
+            --bound_type ${bound_type} \
+            --partial_scaling ${partial_scaling} \
+            --sigma_scaling ${sigma_scaling} \
+            --eps ${eps} \
+            --min_samples ${min_samples} \
+            --range_scale ${range_scale} \
+            --minmax ${minmax} \
+            --noise_method ${noise_method} \
+            --random_view_num ${random_view_num} \
+            --nn_start_iter ${nn_start_iter} \
+            --nn_derivatives_iter ${nn_derivatives_iter} \
+            --nn_update_interval ${nn_update_interval} \
+            --antialiasing \
+            --dvoxel_size ${dvoxel_size} \
+            --port $port 
+    done
+done
--- scene_experimental_branch3/gaussian_model.py
+++ scene_experimental_branch3/gaussian_model.py
@@ -422,6 +422,10 @@ class GaussianModel:
             torch.cuda.empty_cache()
             with torch.no_grad():
                 self.update_nnpts(all_views, pipe, bg, align=False)
+            if self.nn_gt_pts is not None:
+                print(f"Iteration {iteration}: updated nn points, total {self.nn_gt_pts.shape[0]} points.")
+            else:
+                print(f"Iteration {iteration}: no nn points found.")
         if self.net.xyz_lowerbound is None or self.net.xyz_upperbound is None:
             return None
         self.net.train()
@@ -528,14 +532,13 @@ class GaussianModel:
 
         gradient = diff_operators.gradient(pred_opacity, net_in)
         opacity_constraint = F.l1_loss(pred_opacity, gt_opacity).mean()
-        color_constraint = F.l1_loss(pred_color, gt_color).mean()
+        # color_constraint = F.l1_loss(pred_color, gt_color).mean()
         normal_constraint = (1. - F.cosine_similarity(gradient, gt_norm, dim=-1)[..., None]).mean()
         # gradient_constraint = torch.abs(gradient.norm(dim=-1) - 1).mean()
         l = opacity_constraint * 3e3 + \
-            color_constraint * 1e3 + \
             normal_constraint * 1e2
-            # normal_constraint * 1e2 + \
             # gradient_constraint * 5e1
+            # color_constraint * 1e3 + \
 
         return l
     
@@ -695,9 +698,9 @@ class GaussianModel:
         return all_pts
     
     def filter_depth_and_align(self, view, pipe, bg, align=True, debug=True, aligndepth=False):
-        # if int(view.image_name.split(".")[0]) in [248, 249]:
-        if int(view.image_name.split(".")[0]) >= 1171:
-            return None
+        # # if int(view.image_name.split(".")[0]) in [248, 249]:
+        # if int(view.image_name.split(".")[0]) >= 1171:
+        #     return None
         align_depth = None
         mask = None
         # render_pkg = render(view, self, pipe, bg, use_trained_exp=False, separate_sh=False)
